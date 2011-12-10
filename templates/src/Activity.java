7fd5e49da3c4417663b6d44e26e8fb2af5209baa
Ben Ridder <ben@ridder.ca>
2011-12-10 14:44:42 -0500

    * fixed templates to work

--- templates/src/Activity.java
+++ templates/src/Activity.java
@@ -1,9 +1,9 @@
-package <PACKAGE>
+package <PACKAGE>;
 
 import android.app.Activity;
 import android.os.Bundle;
 
-public class <CLASS_NAME> extends Activity implements <CLASS_NAME> {
+public class <CLASS_NAME> extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
