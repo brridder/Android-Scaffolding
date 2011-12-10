a4a8b9d786f75dae1cc3487bcb422501d1d3916f
Ben Ridder <ben@ridder.ca>
2011-12-10 15:10:34 -0500

    * added list activity to the templates

---
+++ templates/src/ListActivity.java
@@ -0,0 +1,17 @@
+package <PACKAGE>;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class <CLASS_NAME> extends ListActivity {
+
+    BaseAdapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.<LAYOUT_CLASS_NAME>);
+
+    }        
+
+}
