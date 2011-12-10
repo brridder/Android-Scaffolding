3807e9c575ccf6de0371c20bd960ba0582c36052
Ben Ridder <ben@ridder.ca>
2011-12-10 14:14:30 -0500

    * added a configuration file to store the base package name

--- android-generator.py
+++ android-generator.py
@@ -11,13 +11,17 @@ LAYOUT_CLASS_NAME = "<LAYOUT_CLASS_NAME>"
 TYPE = "<TYPE>"
 
 template_dir = r"./templates"
-values = {PACKAGE : "com.test.test", 
+values = {
+        PACKAGE : "com.test.test", 
         CLASS_NAME : "TestActivity",
         LAYOUT_CLASS_NAME : "test_activity",
         TYPE : "test",
         }
 
 template_list = []
+conf_file_name = r"./android-scaffolding.conf"
+
+default_conf_file_name = r"./Android-Scaffolding/android-scaffolding.conf"
 
 def parse_string(line):
     for k in values.keys():
@@ -58,12 +62,35 @@ def get_template_list():
     #DEBUG
     #print template_list
 
+def load_config():
+    lines = []
+    if (os.path.exists(conf_file_name)):
+        try:
+            f = open(conf_file_name, "r")
+            lines = f.readlines()
+        except:
+            print "ERROR :: Could not open the configuration file"
+    else:
+        try:
+            f = open(default_conf_file_name, "r")
+            lines = f.readlines()
+        except:
+            print "ERROR :: Could not open the default configuration file"
+    parse_config(lines)
+
+def parse_config(lines):
+    for line in lines:
+        param = line.split("=")
+        values["<"+param[0]+">"] = param[1]
+
 def set_parameters(argv):
     # TODO :: figure out how to get the package
     values[TYPE] = argv[0]
-    values[PACKAGE] = "com.xtremelabs.TODO"
     values[CLASS_NAME] = argv[1]
-    values[LAYOUT_CLASS_NAME] = argv[2]
+    if (len(argv) > 2):
+        values[LAYOUT_CLASS_NAME] = argv[2]
+    else:
+        values.pop(LAYOUT_CLASS_NAME)
 
 def generate_files():
     template_item = None
@@ -80,7 +107,18 @@ def generate_files():
 
     for key in template_item.keys():
         item = template_item[key]
-        copy_file(r"./templates/"+item, r"./" + values[CLASS_NAME] + "." + key)
+        out = ""
+        if (item["type"] == "java"): 
+            out = r"./" + item["out"] + values[PACKAGE].replace(".", "/") + "/" + values[CLASS_NAME] + "." + item["type"]
+        elif (key == "layout" && values.get(LAYOUT_CLASS_NAME) != None)):
+            if (values.get(LAYOUT_CLASS_NAME != None)):
+                out = r"./" + item["out"] + values[LAYOUT_CLASS_NAME] + "." + item["type"]
+            else:
+                out = r"./" + item["out"] + item["default"] + "." + item["type"]
+                print "ERROR :: missing layout file name, using template default"
+        else: 
+            out = r"./" + item["out"] + values[CLASS_NAME] + "." + item["type"]
+        copy_file(r"./templates/"+item["in"], out)
 
 def main(argv):
     # DEBUG
@@ -88,10 +126,11 @@ def main(argv):
     print "starting"
     # END_DEBUG 
 
-    if (len(argv) < 3):
+    if (len(argv) < 2):
         usage()
         sys.exit(2)
-
+   
+    load_config()
     set_parameters(argv)
     get_template_list()
     
